a5a4744522a347bc4e3ba92f1aed331baa33872d
Jie <1614094+yagay@users.noreply.github.com>
2026-08-27 07:20:10 +0100

    fix: enable BuildConfig generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,10 @@ android {
         versionName = "0.1.0"
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
