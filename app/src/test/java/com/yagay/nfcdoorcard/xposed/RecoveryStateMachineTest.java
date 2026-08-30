d023ea340a7f759519d4bff75767e3e28279481f
Jie <1614094+yagay@users.noreply.github.com>
2026-08-30 22:06:05 +0100

    test: cover NFC recovery decisions

---
+++ app/src/test/java/com/yagay/nfcdoorcard/xposed/RecoveryStateMachineTest.java
@@ -0,0 +1,37 @@
+package com.yagay.nfcdoorcard.xposed;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RecoveryStateMachineTest {
+    @Test public void verifiedProofNeedsNoRecovery() {
+        assertEquals(RecoveryStateMachine.Action.NONE,
+                RecoveryStateMachine.next(new RecoveryStateMachine.Snapshot(
+                        true, true, true, true, false, 0, 3)));
+    }
+
+    @Test public void controllerReadyPrefersExactReplay() {
+        assertEquals(RecoveryStateMachine.Action.EXACT_REPLAY,
+                RecoveryStateMachine.next(new RecoveryStateMachine.Snapshot(
+                        true, true, false, true, false, 0, 3)));
+    }
+
+    @Test public void missingReplayFallsBackToVendorTrigger() {
+        assertEquals(RecoveryStateMachine.Action.FALLBACK_TRIGGER,
+                RecoveryStateMachine.next(new RecoveryStateMachine.Snapshot(
+                        true, true, false, false, false, 0, 3)));
+    }
+
+    @Test public void exhaustedFallbackMarksFailure() {
+        assertEquals(RecoveryStateMachine.Action.MARK_FAILED,
+                RecoveryStateMachine.next(new RecoveryStateMachine.Snapshot(
+                        true, true, false, false, true, 3, 3)));
+    }
+
+    @Test public void stoppedSimulationNeverRecovers() {
+        assertEquals(RecoveryStateMachine.Action.NONE,
+                RecoveryStateMachine.next(new RecoveryStateMachine.Snapshot(
+                        false, true, false, true, false, 0, 3)));
+    }
+}
